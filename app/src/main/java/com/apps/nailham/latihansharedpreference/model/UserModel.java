eefe1177f7dee2a53158d8f274359ed4118ca2ac
nailham <nurjamanilham99@gmail.com>
2020-05-01 20:27:10 +0700

    Latihan Sharedpreference

---
+++ app/src/main/java/com/apps/nailham/latihansharedpreference/model/UserModel.java
@@ -0,0 +1,40 @@
+package com.apps.nailham.latihansharedpreference.model;
+
+public class UserModel {
+
+    /*
+    Tanggal Pengerjaan : 1-mei-2020
+    Deskripsi Pengerjaan : Mengerjakan class semua yang terdiri class home,login dan register dan interface serta mengisi semua layout
+    NIM : 10117138
+    Nama: Ilham Nurjaman
+    Kelas : IF-4
+    */
+
+    private String username;
+    private String password;
+    private String phone;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ app/src/main/java/com/apps/nailham/latihansharedpreference/utils/UtilStatic.java
@@ -0,0 +1,17 @@
+package com.apps.nailham.latihansharedpreference.utils;
+     /*
+    Tanggal Pengerjaan : 1-mei-2020
+    Deskripsi Pengerjaan : Mengerjakan class semua yang terdiri class home,login dan register dan interface serta mengisi semua layout
+    NIM : 10117138
+    Nama: Ilham Nurjaman
+    Kelas : IF-4
+    */
+
+public interface UtilStatic {
+    // Default Value
+    String DEFAULT_STRING = "";
+    String STRING_0 = "0";
+    String STRING_NULL = "null";
+    int DEFAULT_INT = 0;
+    boolean DEFAULT_BOOLEAN = false;
+}
